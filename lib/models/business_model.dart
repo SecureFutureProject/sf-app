7d893cc4f9030c7fc06d9089ebf22335fe38122a
SecureFutureProject <work.github.project@gmail.com>
2024-10-06 23:55:08 +0000

    Model Update - Business

--- lib/models/business_model.dart
+++ lib/models/business_model.dart
@@ -1,57 +1,73 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'user_model.dart';
 
-class BusinessModel {
-  final String uid;
+class BusinessModel extends UserModel {
   final String businessName;
   final String logoUrl;
   final String contactDescription;
   final String industry;
-  final String email;
-  final String phone;
   final String website;
   final List<String> socialMediaLinks;
 
   BusinessModel({
-    required this.uid,
+    required String id,
     required this.businessName,
+    required String email,
+    required String location,
+    String? phone,
+    required List<String> niches,
     required this.logoUrl,
     required this.contactDescription,
     required this.industry,
-    required this.email,
-    required this.phone,
     required this.website,
     required this.socialMediaLinks,
-  });
+  }) : super(
+          id: id,
+          name: businessName,
+          email: email,
+          location: location,
+          phone: phone,
+          niches: niches,
+          userType: 'Business',
+        );
 
+  @override
   Map<String, dynamic> toMap() {
     return {
-      'uid': uid,
       'businessName': businessName,
       'logoUrl': logoUrl,
       'contactDescription': contactDescription,
       'industry': industry,
-      'email': email,
-      'phone': phone,
       'website': website,
       'socialMediaLinks': socialMediaLinks,
     };
   }
 
+  Map<String, dynamic> toFullMap() {
+    return {
+      ...super.toMap(),
+      ...toMap(),
+    };
+  }
+
   factory BusinessModel.fromMap(Map<String, dynamic> map) {
     return BusinessModel(
-      uid: map['uid'] ?? '',
+      id: map['id'] ?? '',
       businessName: map['businessName'] ?? '',
+      email: map['email'] ?? '',
+      location: map['location'] ?? '',
+      phone: map['phone'],
+      niches: List<String>.from(map['niches'] ?? []),
       logoUrl: map['logoUrl'] ?? '',
       contactDescription: map['contactDescription'] ?? '',
       industry: map['industry'] ?? '',
-      email: map['email'] ?? '',
-      phone: map['phone'] ?? '',
       website: map['website'] ?? '',
       socialMediaLinks: List<String>.from(map['socialMediaLinks'] ?? []),
     );
   }
 
   factory BusinessModel.fromDocument(DocumentSnapshot doc) {
-    return BusinessModel.fromMap(doc.data() as Map<String, dynamic>);
+    final data = doc.data() as Map<String, dynamic>;
+    return BusinessModel.fromMap({...data, 'id': doc.id});
   }
 }
